11ec4778273935aaf4086417b41c3dbe7b7f5a09
insolor <2442833+insolor@users.noreply.github.com>
2025-09-22 11:01:50 +0300

    Add dict_v3.json, update script

--- automation/dict_manifest_update.py
+++ automation/dict_manifest_update.py
@@ -4,16 +4,22 @@ from pathlib import Path
 
 from utils import get_from_url
 
+repository_root = Path(__file__).parent.parent
+DEFAULT_MIRROR = "https://dfint.github.io"
 
-def main():
-    base_dir = Path(__file__).parent.parent  # base directory of the repository
-    dict_json_path = base_dir / "metadata/dict.json"
 
+def update_v1_dict_manifest():
+    print("Updating checksums for dict manifest v1:")
+    dict_json_path = repository_root / "metadata/dict.json"
     manifest = json.loads(dict_json_path.read_text(encoding="utf-8"))
 
     for i, item in enumerate(manifest):
         try:
-            data = get_from_url(item["csv"]) + get_from_url(item["font"]) + get_from_url(item["encoding"])
+            data = (
+                get_from_url(item["csv"])
+                + get_from_url(item["font"])
+                + get_from_url(item["encoding"])
+            )
             checksum = binascii.crc32(data)
         except Exception as ex:
             print(f"Failed on recalculation {item['language']}:\n", ex)
@@ -21,7 +27,38 @@ def main():
             if item.get("checksum") != checksum:
                 manifest[i]["checksum"] = checksum
 
-    dict_json_path.write_text(json.dumps(manifest, indent=2, ensure_ascii=False), encoding="utf-8")
+    dict_json_path.write_text(
+        json.dumps(manifest, indent=2, ensure_ascii=False), encoding="utf-8"
+    )
+
+
+def update_v3_dict_manifest():
+    print("Updating checksums for dict manifest v3:")
+    dict_json_path = repository_root / "metadata/dict_v3.json"
+    manifest = json.loads(dict_json_path.read_text(encoding="utf-8"))
+
+    for i, item in enumerate(manifest):
+        try:
+            data = (
+                get_from_url(DEFAULT_MIRROR + item["csv"])
+                + get_from_url(DEFAULT_MIRROR + item["font"])
+                + get_from_url(DEFAULT_MIRROR + item["encoding"])
+            )
+            checksum = binascii.crc32(data)
+        except Exception as ex:
+            print(f"Failed on recalculation {item['language']}:\n", ex)
+        else:
+            if item.get("checksum") != checksum:
+                manifest[i]["checksum"] = checksum
+
+    dict_json_path.write_text(
+        json.dumps(manifest, indent=2, ensure_ascii=False), encoding="utf-8"
+    )
+
+
+def main():
+    update_v1_dict_manifest()
+    update_v3_dict_manifest()
 
 
 if __name__ == "__main__":
