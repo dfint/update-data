d6baf029667ba864f3e66f298fb38b910c18c212
insolor <2442833+insolor@users.noreply.github.com>
2025-09-25 14:13:26 +0300

    Implement checksum calculation

--- automation/dict_manifest_generate.py
+++ automation/dict_manifest_generate.py
@@ -1,9 +1,14 @@
+import binascii
 from pathlib import Path
 from pydantic import BaseModel, RootModel
 import strictyaml
 
+from utils import get_from_url
+
 current_directory = Path(__file__).parent
 config_path = current_directory / "dict_manifest_config.yaml"
+repo_root = current_directory.parent
+org = "dfint"
 current_repo = "update-data"
 
 
@@ -18,6 +23,7 @@ class DictManifestConfigEntry(BaseModel):
     language: str
     code: str
     files: list[FileInfo]
+    checksum: int | None = None
 
 
 class Config(RootModel):
@@ -29,5 +35,30 @@ def load_config() -> Config:
     return Config.model_validate(yaml.data)
 
 
+def get_file_data(file: FileInfo) -> bytes:
+    if file.repo == current_repo:
+        return (repo_root / file.path).read_bytes()
+    else:
+        url = f"https://raw.githubusercontent.com/{org}/{file.repo}/refs/heads/{file.branch}/{file.path}"
+        return get_from_url(url)
+
+
+def calculate_checksums(config: Config) -> None:
+    for entry in config.root:
+        data = b''
+        for file in entry.files:
+            file_data = get_file_data(file)
+            data += file_data
+
+        entry.checksum = binascii.crc32(data)
+
+
+def main() -> None:
+    config = load_config()
+    calculate_checksums(config)
+    for row in config.root:
+        print(row.language, row.code, row.checksum)
+
+
 if __name__ == "__main__":
-    print(load_config())
+    main()
